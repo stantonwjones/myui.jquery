25ee2b1c7a0fb8d52dca9d4bcf8439fdb76bf00a
Stanton W. Jones <stantonwjones@gmail.com>
2012-07-11 17:42:50 -0400

    add an example for validations to be used

--- validator.js
+++ validator.js
@@ -27,3 +27,36 @@
         if ($input.val() == defaultValue) return false;
     }
 })(jQuery);
+
+
+/* JS used for contact-us form validation on harvard vanguard
+    // Calidates the Contact Us form if it exists on the page
+    function validateContactForm() {
+        $('#contactForm').submit(function() {
+            return validatePresence($(this).find('#name')) && validateEmail($(this).find('#email')) && validateFormat($(this).find('#phone'));
+        });
+
+        function validatePresence($input) {
+            if ($input.val()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        function validateEmail($input) {
+            if ($input.val().match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        function validateFormat($input) {
+            if (!$input.val()) return true;
+            if ($input.val().match(/^\d\.\d{3}\.\d{3}\.\d{4}$/) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    */
