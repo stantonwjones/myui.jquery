95a3a51001319c1971e94c47fd7273559030a5e7
Stanton <stantonwjones@gmail.com>
2012-08-02 08:33:47 -0400

    carousel now working, but need to change the design paradigm.  "cycler"

--- carousel.js
+++ carousel.js
@@ -8,8 +8,9 @@ function Carousel( $carousel, options ) {
     var $carouselWindow;    // Dom object displaying only the desired carousel items.  Basically a wrapper
     var $carousel;          // Actual container of the list of items
     var $carouselItems;     // For carousel items
+    var $originalCarouselItems;
     var numItems;           // Stores the number of unique carousel items
-    var currentPosition;    // Number representing currentPosition TODO: Replace Position w/ Index for symantics
+    var currentPosition = 0;    // Number representing currentPosition TODO: Replace Position w/ Index for symantics
     var viewWidth;          // width which allows us to see items
     var positions = [];     // Array of values for left
     var carouselObj = {};    // Object exposing carosel functionality.  TODO: Replace this with OOP JS
@@ -27,30 +28,68 @@ function Carousel( $carousel, options ) {
     }
 
     var settings = $.extend({}, defaults, options);
+    console.log(settings);
+    init();
 
     /**
      * Initiates carousel
      */
     function init() {
-        $carouselItems = $carousel.children(settings.childSelector);
+        $originalCarouselItems = $carousel.children(settings.childSelector);
+        extend();
+        console.log($carouselItems);
+        numItems = $originalCarouselItems.length;
         calculatePositions();
-        $carouselWindow = $carousel.wrap('div'); // TODO: add other styling indicators to this item
+        $carouselWindow = $carousel.wrap('<div>').parent(); // TODO: add other styling indicators to this item
+        console.log($carouselWindow, positions);
+        var windowWidth = positions[3];
+        $carouselWindow.width(windowWidth);
+        initStyles();
+        currentPosition += numItems;
+        console.log(currentPosition);
+        transition(); // initial transition to first position
+        addCSSTransitions();
     }
 
     /**
      * Clones carousel items for extending carousel.  Call this once in the init function.
      */
     function extend() {
-        $carouselItems.clone()
+        $originalCarouselItems.clone()
         .appendTo($carousel)
         .clone()
         .prependTo($carousel);
+        $carouselItems = $carousel.children(settings.childSelector);
+    }
+
+    /**
+     * Sets the original css values
+     */
+    function initStyles() {
+        var itemHeights = $originalCarouselItems.map(function() {
+            return $(this).height();
+        });
+        var carouselCss = {
+            position: 'relative',
+            height: Math.max.apply(Math, itemHeights)
+        }
+        var carouselItemCss = {
+            position: 'absolute'
+        }
+        var carouselWindowCss = {
+            overflow: 'hidden'
+        }
+
+        $carousel.css(carouselCss);
+        $carouselItems.css(carouselItemCss);
+        $carouselWindow.css(carouselWindowCss);
     }
 
     /**
      * Performs transition to next state of the carousel
      */
     function next() {
+        console.log('next method fired');
         currentPosition++;
         transition();
     }
@@ -67,22 +106,29 @@ function Carousel( $carousel, options ) {
      * Transition business logic.  Includes wraparound logic
      */
     function transition() {
-        $carousel.css({ left: positions[currentPosition] });
+        $carousel.css({ left: ( positions[currentPosition] * -1 ) });
         // Replace the integer "1" and "2" below with a calculated value based on number of items shown in carousel
         // Wraparound logic
         var wraparound = false;
+        console.log(currentPosition);
         if (currentPosition == 1) {
             currentPostion += numItems;
             wraparound = true;
-        } else if (currentPosition == positions.length - 2) {
+        } else if (currentPosition == positions.length - 3) {
             currentPosition -= numItems;
             wraparound = true;
         }
-        if (wraparound) {
-            performWithoutTransition(function() {
-                $carousel.css({ left: positions[currentPosition] });
-            });
-        }
+        window.setTimeout(function() {
+            if (wraparound) {
+                performWithoutCSSTransition( function() {
+                    console.log($carousel);
+                    $carousel.css({
+                        left: (positions[currentPosition] * -1)
+                    });
+                    console.log($carousel);
+                });
+            }
+        }, settings.animationDuration);
     }
 
     /**
@@ -102,36 +148,57 @@ function Carousel( $carousel, options ) {
      * @param {function} func   The function to execute.
      * @return {null}
      */
-    function performWithoutTransition(func) {
+    function performWithoutCSSTransition(func) {
         removeCSSTransitions();
+        /*
         func();
+        console.log($carousel);
         addCSSTransitions();
+        */
+    }
+    function listenForTransitions() {
+        $carousel.on('DOMAttrModified', function() {
+            if (!cssTransitions) {
+            console.log($carousel);
+            func();
+            addCSSTransitions();
+            console.log($carousel);
+        })
     }
 
     /**
      * Removes the css transition from the carousel
      */
-    function removeCSSTransitions() {
-        $carousel.find('.carousel-container').first().css({ transitionProperty: 'none' });
+    window.removeCSSTransitions = function() {
+        $carousel.css({
+            '-webkit-transition': 'none',
+            '-moz-transition': 'none'
+        });
     }
 
     /**
      * Adds css transitions to the carousel
      */
     function addCSSTransitions() {
-        $carousel.find('.carousel-container').first().css({ transitionProperty: 'left' });
+        var seconds = settings.animationDuration / 1000 + 's';
+        console.log(seconds);
+        $carousel.css({
+            mozTransitionProperty: 'left',
+            webkitTransitionProperty: 'left',
+            mozTransitionDuration: seconds,
+            webkitTransitionDuration: seconds
+        });
     }
 
     /**
      * Calculates values for the positions array based on width of elements in carousel.
      */
     function calculatePositions() {
-        $carousel.find('.carousel-container .carousel-item').each(function(i, v) {
-            var value = $(this).width();
-            if (positions[i - 1]) {
-                value += positions[i - 1];
-            }
+        var value = 0;
+        $carouselItems.each(function() {
             positions.push(value);
+            $(this).css({ left: value });
+            value += $(this).width();
         });
     }
 
